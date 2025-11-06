57ca7460aa405f4562ffc051a604fff70d13e3ce
MinhTri676 <caominhtri1604@gmail.com>
2025-11-06 07:35:56 +0700

    modify controller

--- src/java/constant/MessageConstant.java
+++
@@ -1,7 +0,0 @@
-package constant;
-
-public class MessageConstant {
-    public static final String LOGIN_FAILED = "Login Failed";
-    public static final String DELETE_SUCCESS = "Delete Successfully";
-    public static final String DELETE_FAILED = "Delete Failed";
-}
